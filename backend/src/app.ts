380d8ca75963ccdc4eec2b5238a79c26293c32af
maixp089 <maixp089@yahoo.co.jp>
2025-09-03 15:18:38 +0900

    fix: CORS設定の修正

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,24 +1,30 @@
-import express from "express";
+import express from 'express';
 import cors from 'cors';
-import portfolioRouter from "./routes/portfolio";
-import skillRouter from "./routes/skill";
-import contactRouter from "./routes/contact";
+import portfolioRouter from './routes/portfolio';
+import skillRouter from './routes/skill';
+import contactRouter from './routes/contact';
 const app = express();
 
-app.use(cors({
-  origin: [
-  'http://localhost:3000', 
-  "https://portfolio-delta-nine-d809vhhml9.vercel.app"
-  ],
-  credentials: true 
-}));
+app.use(
+  cors({
+    origin: [
+      'http://localhost:3000',
+      'https://portfolio-delta-nine-d809vhhml9.vercel.app',
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }),
+);
+// preflight対応
+app.options("*", cors());
+
 app.use(express.json());
-app.use("/api/portfolios", portfolioRouter);
-app.use("/api/skills", skillRouter);
-app.use("/api/contacts", contactRouter);
+app.use('/api/portfolios', portfolioRouter);
+app.use('/api/skills', skillRouter);
+app.use('/api/contacts', contactRouter);
 app.use('/uploads', express.static('uploads'));
 
-
 app.listen(4000, () => {
-  console.log("APIサーバー起動中！");
+  console.log('APIサーバー起動中！');
 });
